d78cb802666bfc6c320925d005cc0e922409a4af
raivolalala <lallo.raivo@gmail.com>
2020-05-16 22:46:21 +0300

    modified send_text.sh

--- send_text.sh
+++ send_text.sh
@@ -1,6 +1,8 @@
 # Katrina +37124001669
 # Gustavs +37129236614
 # Maris +37126779409
+# Sigis +37129383070
+# Bites īsziņu centra nr. +37125850115
 curl -X POST https://textbelt.com/text \
        --data-urlencode phone='' \
        --data-urlencode message='' \
