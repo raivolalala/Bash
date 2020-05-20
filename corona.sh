b460476a2ddc5edab9bf87b63d0e7f026a9e8333
raivolalala <lallo.raivo@gmail.com>
2020-05-20 15:45:08 +0300

    modified corona script

--- corona.sh
+++ corona.sh
@@ -4,5 +4,5 @@
 	"$(date '+%Y-%m-%d')" ] &&
        curl -s https://corona-stats.online > ~/.cache/corona	
 grep "Latvia" ~/.cache/corona |
-	sed "s/\s*//g; s/║//g ; s/│/;/g" |
-	awk -F';' '{print $2"\n" "Total cases: "$3"\n" "Deaths: "$5"\n" "Recovered: "$7"\n" "Active cases: "$8"\n"}' && rm ~/.cache/corona
+	sed "s/\s*//g; s/║//g ; s/│/;/g ; s/▲//g" |
+	awk -F';' '{print $2"\n" "Total cases: "$3"\n" "New cases: "$4"\n" "Deaths: "$5"\n" "Recovered: "$7"\n" "Active cases: "$8"\n"}' && rm ~/.cache/corona
