5d3b28bc4f715dc46d43547c0920412c44309743
raivolalala <lallo.raivo@gmail.com>
2020-05-21 09:21:42 +0300

    updated combine_audio_picture.sh with filenames as command line arguments

--- combine_audio_picture.sh
+++ combine_audio_picture.sh
@@ -1,1 +1,5 @@
-ffmpeg -loop 1 -i image.jpg -i audio.wav -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest out.mp4
+#!/bin/bash
+# $1 - full path to your existing picture file; 
+# $2 - full path to your existing audio file; 
+# $3 - full path to your new output video file.
+ffmpeg -loop 1 -i $1 -i $2 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest $3
