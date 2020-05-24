47c3aa43a293ed225c3cce0d90271477e8a7df9d
raivolalala <lallo.raivo@gmail.com>
2020-05-24 14:22:10 +0300

    updated send_text.sh, combine_audio_picture.sh for readability

--- combine_audio_picture.sh
+++ combine_audio_picture.sh
@@ -2,4 +2,5 @@
 # $1 - full path to your existing picture file; 
 # $2 - full path to your existing audio file; 
 # $3 - full path to your new output video file.
-ffmpeg -loop 1 -i $1 -i $2 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest $3
+ffmpeg -loop 1 -i $1 -i $2 -c:v libx264 -tune stillimage \
+	-c:a aac -b:a 192k -pix_fmt yuv420p -shortest $3
--- send_text.sh
+++ send_text.sh
@@ -1,7 +1,3 @@
-# Katrina +37124001669
-# Gustavs +37129236614
-# Maris +37126779409
-# Sigis +37129383070
 # Bites īsziņu centra nr. +37125850115
 curl -X POST https://textbelt.com/text \
        --data-urlencode phone='' \
