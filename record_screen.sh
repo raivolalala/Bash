47fa63267a252d71342f123528c566f5d75da8ba
raivolalala <lallo.raivo@gmail.com>
2020-06-04 17:07:14 +0300

    modded record_screen.sh for manual input resolution

--- record_screen.sh
+++ record_screen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#INRES="1366x768" -manual
+#INRES_A=`xdpyinfo | grep 'dimensions:'|awk '{print $2}'` -automatic 
 ffmpeg -y -f alsa -i default \
-	-f x11grab -s `xdpyinfo | grep 'dimensions:'|awk '{print $2}'` -r 25 \
+	-f x11grab -s "$INRES" -r 25 \
 	-i :0.0 -f video4linux2 \
 	-i /dev/video0 -filter_complex '[2:v]scale=380:-1[cam];[1:v][cam]overlay=W-w-8:H-h-8:x=990:y=0' \
 	-c:a mp3 \
