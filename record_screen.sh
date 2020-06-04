3c70da6d3fa6ebbc87e39907de312045bfca2fdf
raivolalala <lallo.raivo@gmail.com>
2020-06-04 16:11:00 +0300

    modded record_screen.sh

--- record_screen.sh
+++ record_screen.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Replace $1 with your desired output file name.
 ffmpeg -y -f alsa -i default \
 	-f x11grab -s `xdpyinfo | grep 'dimensions:'|awk '{print $2}'` -r 25 \
 	-i :0.0 -f video4linux2 \
 	-i /dev/video0 -filter_complex '[2:v]scale=380:-1[cam];[1:v][cam]overlay=W-w-8:H-h-8:x=990:y=0' \
 	-c:a mp3 \
-	-qscale 0 $1 
+	-qscale 0 out.mp4 
