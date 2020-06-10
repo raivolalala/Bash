14294e6246f323a1e4a043ade56caf2eb771626e
raivolalala <lallo.raivo@gmail.com>
2020-06-11 01:16:22 +0300

    added clock to hacktop_status.sh

--- hacktop_status.sh
+++ hacktop_status.sh
@@ -6,6 +6,7 @@ bat_status=`cat /sys/class/power_supply/BAT0/status | sed 's/.*/\L\0/g'`
 temp=`sensors | awk '/^Core 0/ {print "CPU temp: "$3}'`
 disk=`df -h | grep '/dev/sda1' | awk '{print "Free - " $4}'`
 
+echo Time: $(date +%H:%M)
 echo Battery: $capacity%, $bat_status;
 echo $temp
 echo SSD: $disk
