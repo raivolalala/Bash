fae0b7bf264cf5b6684359077aeb310a94fddb6e
raivolalala <lallo.raivo@gmail.com>
2020-06-12 00:14:57 +0300

    modded hacktop_status.sh

--- hacktop_status.sh
+++ hacktop_status.sh
@@ -4,9 +4,9 @@
 capacity=`cat /sys/class/power_supply/BAT0/capacity`
 bat_status=`cat /sys/class/power_supply/BAT0/status | sed 's/.*/\L\0/g'`
 temp=`sensors | awk '/^Core 0/ {print "CPU temp: "$3}'`
-disk=`df -h | grep '/dev/sda1' | awk '{print "Free - " $4}'`
+disk=`df -h | grep '/dev/sda1' | awk '{print $4}'`
 
 echo Time: $(date +%H:%M)
 echo Battery: $capacity%, $bat_status;
 echo $temp
-echo SSD: $disk
+echo SSD free: $disk
