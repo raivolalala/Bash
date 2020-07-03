f9c0630a5601a89d9e715d2804a95aad4d611e5f
raivolalala <lallo.raivo@gmail.com>
2020-07-03 15:41:35 +0300

    optimized fade_video.sh

--- fade_video.sh
+++ fade_video.sh
@@ -2,6 +2,4 @@
 
 # $1 - your input video
 # Note: have to create params for custom fade in/out times
-ffmpeg -i $1 -vf "fade=t=in:st=0:d=3" -c:a copy fade_in.mp4
-ffmpeg -i fade_in.mp4 -vf "fade=t=out:st=0:d=3" -c:a copy out_fade.mp4
-rm fade_in.mp4
+ffmpeg -i $1 -vf "fade=t=in:st=0:d=3,fade=t=out:st=4:d=2" -c:a copy out_faded.mp4
