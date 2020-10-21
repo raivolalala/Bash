5898a7528661c4cc2fd51dcf064ad52ddbce0da8
raivolalala <lallo.raivo@gmail.com>
2020-10-22 00:51:04 +0300

    some random shit

--- send_text.sh
+++ send_text.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Bites īsziņu centra nr. +37125850115
 curl -X POST https://textbelt.com/text \
-       --data-urlencode phone='' \
-       --data-urlencode message='' \
+       --data-urlencode phone='+37128633146' \
+       --data-urlencode message='atmet whatsappa screenu tagad' \
        -d key=textbelt
