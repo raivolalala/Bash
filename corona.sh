c7f2ebaabcd63999c1eb0676a8cf7567dae1ccfc
raivolalala <lallo.raivo@gmail.com>
2020-10-22 01:46:35 +0300

    adeed historization to corona.sh + some rubbish in send_text.sh

--- corona.sh
+++ corona.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-[ "$(stat -c %y ~/.cache/corona 2>/dev/null| cut -d ' ' -f1)" != \
+now=$(date +"%Y-%m-%d")
+folder="home/raivo/Documents/COVID19/"
+[ "$(stat -c %y ~/.cache/corona_$now 2>/dev/null | cut -d ' ' -f1)" != \
 	"$(date '+%Y-%m-%d')" ] &&
-       curl -s https://corona-stats.online > ~/.cache/corona	
-grep "Latvia" ~/.cache/corona |
+       curl -s https://corona-stats.online > ~/.cache/corona_$now	
+grep "Latvia" ~/.cache/corona_$now |
 	sed "s/\s*//g; s/║//g ; s/│/;/g ; s/▲//g" |
-	awk -F';' '{print $2"\n" "Total cases: "$3"\n" "New cases: "$4"\n" "Deaths: "$5"\n" "New deaths: "$6"\n" "Recovered: "$7"\n" "Active cases: "$8"\n"}' && rm ~/.cache/corona
+	awk -F';' '{print $2"\n"\
+       	"Total cases: "$3"\n"\ 
+	"New cases: "$4"\n"\
+       	"Deaths: "$5"\n"\
+       	"New deaths: "$6"\n"\
+       	"Recovered: "$7"\n"\
+       	"Active cases: "$8"\n"}'
+#rm ~/.cache/corona
--- send_text.sh
+++ send_text.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
 # Bites īsziņu centra nr. +37125850115
+
+# Long string var definition below.
+# Note the long string var usage further on - in double quotes.
+# Double quotes remove newline char after backslash.
+
+nmbr='+37128633146'
+msg='atmet whatsappā screennu tagad \
+	jo šitā sms neietilpst \
+	vienā jobnutā rindinā'
+
 curl -X POST https://textbelt.com/text \
-       --data-urlencode phone='+37128633146' \
-       --data-urlencode message='atmet whatsappa screenu tagad' \
+       --data-urlencode phone=$nmbr \
+       --data-urlencode message="$msg" \
        -d key=textbelt
