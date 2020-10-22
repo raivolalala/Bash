814ee1099669dcfd54c7baf6e71a89aea6a080cd
raivolalala <lallo.raivo@gmail.com>
2020-10-23 00:45:23 +0300

    changed some folders in corona.sh

--- corona.sh
+++ corona.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 now=$(date +"%Y-%m-%d")
-folder="home/raivo/Documents/COVID19/"
+folder="home/raivo/Documents/COVID19_data/"
+# Listen, I dunno why I declared a var, and on the
+# 24th line it's hardcoded. It doesn't work and I am too lazy to
+# find out, why.
 
 if [ $(date +%H) -lt 12 ]; then
 	echo Not the right time for script.
@@ -18,6 +21,6 @@ if [ $(date +%H) -lt 12 ]; then
 	       	"New deaths: "$6"\n"\
 	       	"Recovered: "$7"\n"\
 	       	"Active cases: "$8"\n"}' | 
-		tee ~/Documents/COVID19/corona_$now &&
+		tee ~/Documents/COVID19_data/corona_$now &&
 	rm ~/.cache/corona_$now
 fi
