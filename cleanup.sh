a99f9d8fe8a36760fd62b0f6c702ac82fb4d759e
raivolalala <lallo.raivo@gmail.com>
2020-10-23 00:20:04 +0300

    scraped some data.. whatevz

--- cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-folder_list=`locate $1`
-for i in $folder_list
-do
-	rm $i -rf;
-done
--- stream_to_twitch.sh
+++ stream_to_twitch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Note that vars below are hard-coded for my own specs.
+# Don't be lazy as I am, think how to improve it.
+# And just fucking don't be a bitch, I am still learning all this shit.
+
 streaming() {
 	INRES="1366x768" # input resolution
 	OUTRES="1366x768" # output resolution
@@ -11,7 +15,7 @@ streaming() {
 	QUALITY="ultrafast"  # one of the many FFMPEG preset
 	AUDIO_RATE="44100"
 	#STREAM_KEY="$1"
-	STREAM_KEY="live_496104237_gY9wixUV1VoMxTTMEwt0fGUQtBBSqx"
+	STREAM_KEY=$(cat ~/Documents/stream_key)
 	SERVER="live-sjc" # twitch server in California, see http://bashtech.net/twitch/ingest.php to change 
      
 	ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f alsa -i pulse -f flv -ac 2 -ar $AUDIO_RATE \
