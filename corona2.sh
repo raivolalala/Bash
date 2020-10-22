500149a43654f36bf166dc71e6e90adf02f1ac18
raivolalala <lallo.raivo@gmail.com>
2020-10-22 10:05:33 +0300

    removed bullshit

--- corona2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[ "$(stat -c %y ~/.cache/corona 2>/dev/null| cut -d ' ' -f1)" != \
-	"$(date '+%Y-%m-%d')" ] &&
-       curl -s https://corona-stats.online > ~/.cache/corona	
-grep "Latvia" ~/.cache/corona |
-	sed "s/\s*//g; s/║//g ; s/│/;/g ; s/▲//g" |
-	cut -d';' -f2 -f3 -f4 &&
-	rm ~/.cache/corona
