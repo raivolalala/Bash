b8b748d7c3f53ad781db6979f813d1c015c0812a
raivolalala <lallo.raivo@gmail.com>
2020-11-07 13:37:34 +0200

    everythang commit msg omg omg

--- show_weather.sh
+++ show_weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# TODO: Throws 404... again... and again...
+# TODO: Throws 404... again... and again... aaaand again...
 weather='https://www.accuweather.com/akam/11\
 	/pixel_651fbf7e?a=dD0yMmY4NjRkZjM5\
 	OTk0MTc5MDRjYjViN2Y3ZmM5MWRkZDA1M2JhODMyJmpzPW9mZg=='
