9a6152c22cf01871a1a89f48f529429f5a3fb1d2
raivolalala <lallo.raivo@gmail.com>
2020-11-10 23:21:00 +0200

    modded some shit in split_audio.sh

--- split_audio.sh
+++ split_audio.sh
@@ -3,4 +3,4 @@
 file1="/home/raivo/Audio/Music/The Midnight/the_midnight-monsters_tracklist"
 file2="/home/raivo/Audio/Music/The Midnight/The Midnight - Monsters (2020).mp3"
 
-sed -n 1p "$file1"
+sed -n 1p "$file1" | head -c 8
