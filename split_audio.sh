427f7e0a0f5b61ce6dcae9220b86c32a2f3acaa8
raivolalala <lallo.raivo@gmail.com>
2020-11-10 23:04:22 +0200

    added split_audio.sh, only beginning

---
+++ split_audio.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+file1="/home/raivo/Audio/Music/The Midnight/the_midnight-monsters_tracklist"
+file2="/home/raivo/Audio/Music/The Midnight/The Midnight - Monsters (2020).mp3"
+
+sed -n 1p "$file1"
