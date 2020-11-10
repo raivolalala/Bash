4ad0685ec114caaeb062b23637521f1c03a0454e
raivolalala <lallo.raivo@gmail.com>
2020-11-10 23:28:10 +0200

    modded some shit again in that script

--- split_audio.sh
+++ split_audio.sh
@@ -3,4 +3,9 @@
 file1="/home/raivo/Audio/Music/The Midnight/the_midnight-monsters_tracklist"
 file2="/home/raivo/Audio/Music/The Midnight/The Midnight - Monsters (2020).mp3"
 
-sed -n 1p "$file1" | head -c 8
+time1=$(sed -n 1p "$file1" | head -c 8)
+time2=$(sed -n 2p "$file1" | head -c 8)
+
+echo ${time2}
+
+ffmpeg -ss ${time1} -i $file2 -to ${time2} out.mp3
