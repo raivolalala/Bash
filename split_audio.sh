5c449c2da8224fa27d8c4d1f1696211a25a3f926
raivolalala <lallo.raivo@gmail.com>
2020-11-23 19:30:31 +0200

    modded some shit

--- split_audio.sh
+++ split_audio.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-tracklist=$1
-music=$2
 
 # Timestamps - this file needs to start with %H:%m:%s, so
 # please adjust it accordingly.
 # TODO: need to create a calculation for song lengths.
-time1=$(sed -n 1p "$tracklist" | head -c 8)
-time2=$(sed -n 2p "$tracklist" | head -c 8)
-time3=$(sed -n 3p "$tracklist" | head -c 8)
-time4=$(sed -n 4p "$tracklist" | head -c 8)
+time1=$(sed -n 1p $1 | head -c 8)
+time2=$(sed -n 2p $1 | head -c 8)
+time3=$(sed -n 3p $1 | head -c 8)
+time4=$(sed -n 4p $1 | head -c 8)
 
 # Track names
-track1=$(sed -n 1p "$tracklist" | cut -c10-)
-track2=$(sed -n 2p "$tracklist" | cut -c10-)
-track3=$(sed -n 3p "$tracklist" | cut -c10-)
+track1=$(sed -n 1p $1 | cut -c10-)
+track2=$(sed -n 2p $1 | cut -c10-)
+track3=$(sed -n 3p $1 | cut -c10-)
 
 # Cut the file into tracks
-ffmpeg -ss ${time1} -i "$music" -to ${time2} "${track1}.mp3"
-ffmpeg -ss ${time2} -i "$music" -to ${time3} "${track2}.mp3"
-ffmpeg -ss ${time3} -i "$music" -to ${time4} "${track3}.mp3"
+ffmpeg -ss ${time1} -i $2 -to ${time2} "${track1}.mp3"
+ffmpeg -ss ${time2} -i $2 -to ${time3} "${track2}.mp3"
+ffmpeg -ss ${time3} -i $2 -to ${time4} "${track3}.mp3"
