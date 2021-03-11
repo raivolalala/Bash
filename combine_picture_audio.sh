642e8596a2103bef90234808f89a6fdcf884b3b7
raivolalala <lallo.raivo@gmail.com>
2021-03-11 11:51:50 +0200

    added faster processing time

--- combine_picture_audio.sh
+++ combine_picture_audio.sh
@@ -2,4 +2,4 @@
 # $1 - full path to your existing picture file; 
 # $2 - full path to your existing audio file; 
 ffmpeg -loop 1 -i $1 -i $2 -c:v libx264 -tune stillimage \
-	-c:a aac -b:a 192k -pix_fmt yuv420p -shortest out.mp4
+	-c:a copy -b:a 192k -pix_fmt yuv420p -shortest out.mp4
